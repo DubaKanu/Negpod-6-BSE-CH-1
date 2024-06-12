161c55ec3f7c81d0a2fd40afc51900c7188fee3d
Fitsum Berhane <f.haile@alustudent.com>
2024-06-12 09:38:21 +0200

    Scripts for question 2

---
+++ Move-to-directory.sh
@@ -0,0 +1,8 @@
+!#/bin/bash
+# A script to move the 4 files to the directory Negpod_6-q1
+
+# Create the directory Negpod_6-q1
+mkdir Negpod_6q1
+
+# Command to move the 4 files from question 1.
+mv Negpod_6-q1
