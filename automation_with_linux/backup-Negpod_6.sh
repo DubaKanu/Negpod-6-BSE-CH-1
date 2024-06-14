05aeed73daf1ba038bbf7c39abea5fbc753b87de
octaviusthe3rd <admirechaga@gmail.com>
2024-06-14 04:41:34 -0700

    timestamp for backup

--- automation_with_linux/backup-Negpod_6.sh
+++ automation_with_linux/backup-Negpod_6.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-#Backup the negpod_id-q1 directory to the remote server
+# Backup the Negpod_6-q1 directory to the remote server
 
 directory="Negpod_6-q1"
-remote_host="ec1ebc754f75.a5efec7a.alu-cod.online"
-remote_user="ea1d8f222b11d7be18f6"
+remote_host="8db086299c2d.51efc529.alu-cod.online"
+remote_user="8db086299c2d"
 remote_directory="/summative/0524-2024m"
 
 if [[ -d $directory ]]; then
+    
+    timestamp=$(date +"%Y%m%d%H%M%S")
 
-	rsync -avz $directory $remote_user@$remote_host:$remote_directory
-	echo "Backup to remote server completed successfully."
+
+    scp -r "$directory" "$remote_user@$remote_host:$remote_directory/${directory}_$timestamp"
+    
+    echo "Backup to remote server completed successfully."
 else
     echo "Directory $directory does not exist."
 fi
