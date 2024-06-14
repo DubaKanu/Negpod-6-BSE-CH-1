2587e27c4195c9f8f894b938689c119d9d87de4a
octaviusthe3rd <admirechaga@gmail.com>
2024-06-14 03:52:53 -0700

    backup code success

--- automation_with_linux/backup-Negpod_6.sh
+++ automation_with_linux/backup-Negpod_6.sh
@@ -3,13 +3,12 @@
 
 directory="Negpod_6-q1"
 remote_host="ec1ebc754f75.a5efec7a.alu-cod.online"
-remote_user="ec1ebc754f75"
-remote_password="ea1d8f222b11d7be18f6"
+remote_user="ea1d8f222b11d7be18f6"
 remote_directory="/summative/0524-2024m"
 
 if [[ -d $directory ]]; then
 
-	sshpass -p $remote_password scp -r $directory $remote_user@$remote_host:$remote_directory
+	rsync -avz $directory $remote_user@$remote_host:$remote_directory
 	echo "Backup to remote server completed successfully."
 else
     echo "Directory $directory does not exist."
