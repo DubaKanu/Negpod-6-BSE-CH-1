00ea06bb2441f66400e41f02383abba17c447648
Millowise01 <l.carew@alustudent.com>
2024-06-19 03:28:07 -0700

    Added a comments that explain the code - Move-to-directory.sh

--- automation_with_linux/Move-to-directory.sh
+++ automation_with_linux/Move-to-directory.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# directory="Negpod_6-q1" : To assings the string "Negpod_6-q1" to a virables named directory.
+
 directory="Negpod_6-q1"
+
+#To check if the directory specified by the $directory variable does NOT exist using the -d operator for directory check.
 
 if [[ ! -d $directory ]]; then
     mkdir $directory
 
+# 
     mv ../shell_programming/main.sh ../shell_programming/students-list_1023.txt ../shell_programming/select-emails.sh ../shell_programming/student-emails.txt $directory
 
     echo "Files moved successfully"
