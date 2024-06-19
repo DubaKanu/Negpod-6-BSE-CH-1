555ef86c29c8de0473c6691c57c45e9dc29d7a07
Millowise01 <l.carew@alustudent.com>
2024-06-19 02:52:33 -0700

    Added more comments to explain the code _ select-emails.sh

--- shell_programming/select-emails.sh
+++ shell_programming/select-emails.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 #a command to select just the emails of the student
+#cut : this command extract specific parts of each line from a text
+#-d ',' : to tell cut to use a comma (,) as the delimiter to seperate fields within each line.
+#-f1 : Option specifies that we only want the first field before the comma from each line.
+
 cut -d',' -f1 students-list_1023.txt | sort > student-emails.txt
 
 echo "Student emails saved to student-emails.txt."
