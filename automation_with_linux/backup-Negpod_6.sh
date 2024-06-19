bfee09d6a41a48f98de2aac012c916339de28f70
Millowise01 <l.carew@alustudent.com>
2024-06-19 03:43:33 -0700

    Added few comment to explain part of main_2.sh

--- automation_with_linux/backup-Negpod_6.sh
+++ automation_with_linux/backup-Negpod_6.sh
@@ -6,6 +6,10 @@ remote_host="ec1ebc754f75.a5efec7a.alu-cod.online"
 remote_user="ec1ebc754f75"
 remote_directory="/summative/0524-2024m"
 
+# if [[ -d $directory ]]; then: This line starts an if statement. It checks if the directory specified by the variable $directory exists.
+# -d: This is a test operator that checks if the argument is a directory.
+# $directory: This refers to the value stored in the variable directory. It's assumed the script receives the directory path as input somehow
+
 if [[ -d $directory ]]; then
     
     timestamp=$(date +"%Y%m%d%H%M%S")
